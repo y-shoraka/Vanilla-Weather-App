ad4dd618e3c80e7d29e2c79c7e1dfcb9fecdabff
y-shoraka <shorakayasaman@gmail.com>
2022-12-25 22:23:57 +0330

    unit conversion

--- Src/index.js
+++ Src/index.js
@@ -12,8 +12,21 @@ if (minutes < 10) {
   minutes ="0"+ minutes;
 }
 document.querySelector("#minute").innerHTML = date.getMinutes();
+function convertToF(){
+    let F = (celsius * 9/5) + 32 ;
+    document.querySelector("#temp").innerHTML = Math.round(F);
+
+}
+function convertToC(){
+    
+    document.querySelector("#temp").innerHTML =Math.round(celsius)  ; 
+}
+
+document.querySelector("#F").addEventListener("click" , convertToF);
+document.querySelector("#C").addEventListener("click" , convertToC);
 
 function DisplayTemp(response) {
+    celsius = response.data.temperature.current;
   document.querySelector("#temp").innerHTML = Math.round(
     response.data.temperature.current
   );
@@ -43,5 +56,5 @@ function submitHandler() {
   document.querySelector(".cityName").innerHTML = searchedCity ; 
   search(searchedCity);
 }
-
+let celsius = null ; 
 document.querySelector("#submit").addEventListener("click", submitHandler);
