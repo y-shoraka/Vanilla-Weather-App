f852c820b292e7cf09827f25545942e262716785
y-shoraka <shorakayasaman@gmail.com>
2022-12-25 21:49:51 +0330

    added search engine

--- Src/index.js
+++ Src/index.js
@@ -1,33 +1,47 @@
-let Days = [ "Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Friday","Satuday"  ]
-let date = new Date(); 
-let CurrentDay = date.getDay() ;
-document.querySelector("#day").innerHTML = Days[CurrentDay] ;
+let Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Satuday"];
+let date = new Date();
+let CurrentDay = date.getDay();
+document.querySelector("#day").innerHTML = Days[CurrentDay];
 let hours = date.getHours();
-if (hours < 10){
-    hours = `0${hours}`
+if (hours < 10) {
+  hours = `0${hours}`;
 }
-document.querySelector("#hour").innerHTML = hours+":" ;
- let minutes =  date.getMinutes();
- if (minutes <10 ){
-    minutes = `0${minutes}`
- }
+document.querySelector("#hour").innerHTML = hours + ":";
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes ="0"+ minutes;
+}
 document.querySelector("#minute").innerHTML = date.getMinutes();
-
 
 function DisplayTemp(response) {
   document.querySelector("#temp").innerHTML = Math.round(
     response.data.temperature.current
   );
-  document.querySelector("#description").innerHTML = response.data.condition.description;
-  document.querySelector("#humidity").innerHTML = response.data.temperature.humidity+"%";
-  document.querySelector("#wind").innerHTML = `  Wind: ${response.data.wind.speed}Km/hr` ; 
-  document.querySelector("#img").setAttribute("src" , response.data.condition.icon_url)
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity + "%";
+  document.querySelector(
+    "#wind"
+  ).innerHTML = `  Wind: ${response.data.wind.speed}Km/hr`;
+  document
+    .querySelector("#img")
+    .setAttribute("src", response.data.condition.icon_url);
   console.log(response);
-
 }
 
-let key = "d622ab03edofbbtc80f362a442d6777c";
-let city = "tehran";
-let ApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+function search(city) {
+  let key = "d622ab03edofbbtc80f362a442d6777c";
 
-axios.get(ApiUrl).then(DisplayTemp);
+  let ApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+
+  axios.get(ApiUrl).then(DisplayTemp);
+}
+
+function submitHandler() {
+  let searchedCity = document.querySelector("#searchInput").value;
+  document.querySelector(".cityName").innerHTML = searchedCity ; 
+  search(searchedCity);
+}
+
+document.querySelector("#submit").addEventListener("click", submitHandler);
